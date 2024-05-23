d4e6b377bca8396b47b54d43935c486237f410cb
Drownika G <drownikag@gmail.com>
2024-05-23 11:41:34 +0530

    first commit

---
+++ src/component/NetflixFooterComponent.js
@@ -0,0 +1,50 @@
+
+
+export default function NetflixFooterComponent()
+{
+    return(
+        <>
+        Questions? Call 000-800-919-1694
+        <div className="row">
+            <div className="col">
+                <ul className="list-unstyled">
+                    <li>FAQ</li>
+                    <li>Investor Relations</li>
+                    <li>Media Center</li>
+                    <li>privacy</li>
+                </ul>
+
+            </div>
+            <div className="col">
+                <ul className="list-unstyled">
+                    <li>Help center</li>
+                    <li>Jobs</li>
+                    <li>Cookie Preferences</li>
+                    <li>Legal Notice</li>
+                </ul>
+
+            </div>
+            <div className="col">
+            <ul className="list-unstyled">
+                    <li>Account</li>
+                    <li>Ways To Waatch</li>
+                    <li>Only on Netflix</li>
+                    <li>Corporate Information</li>
+                </ul>
+
+            </div>
+            <div className="col">
+            <ul className="list-unstyled">
+                    <li>Career</li>
+                    <li>Terms of Use</li>
+                    <li>Speed Test</li>
+                    <li>Contact Us</li>
+                </ul>
+
+            </div>
+
+        </div>
+        
+        </>
+    );
+}
---
+++ src/component/NetflixMainComponent.js
@@ -0,0 +1,13 @@
+
+export default function NetflixMainComponent()
+{
+    return(
+        <>
+        <div className="text-center w-100">
+            <h1>Unlimited movies , TV Shows and More.</h1>
+            <h3>Watch Anywhere. Cancel Anytime</h3>
+        </div>
+        
+        </>
+    );
+}
